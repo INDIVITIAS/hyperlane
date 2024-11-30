2ca36ad8d70f7d5f7f1481057b196cc43c7f149f
IN DIVITIAS <olegua2020@gmail.com>
2024-11-30 03:06:48 +0200

    Update hyper.sh

--- hyper.sh
+++ hyper.sh
@@ -10,64 +10,71 @@ install_git() {
     fi
 }
 
-# Функция для установки и запуска ноды
-install_and_run_node() {
-    # Обновление пакетов и установка необходимых инструментов
+# Функция для установки и настройки ноды
+install_and_configure_node() {
+    echo "Устанавливаем и настраиваем ноду..."
     sudo apt-get update && sudo apt-get install -y \
     curl iptables build-essential git wget jq make gcc nano tmux htop nvme-cli \
     pkg-config libssl-dev libgmp3-dev tar clang bsdmainutils ncdu unzip llvm \
     libudev-dev protobuf-compiler cmake cargo openssl
-
-    # Установка Rust
+    
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    source $HOME/.cargo/env
-
-    # Установка Foundry
     curl -L https://foundry.paradigm.xyz | bash
     source ~/.bashrc
     foundryup
-
-    # Установка NVM (Node Version Manager) и Node.js LTS
+    
     curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
     source ~/.nvm/nvm.sh
     nvm install --lts
     nvm use --lts
-
-    # Проверка версии npm и установка Hyperlane CLI
+    
     npm --version
     npm install -g @hyperlane-xyz/cli
     hyperlane --version
-
-    # Установка screen
+    
     sudo apt install -y screen
+    
+    hyperlane core init --advanced | while read line; do
+        echo "$line"
+        if echo "$line" | grep -q "✅ Successfully created new core deployment config."; then
+            echo "Выполняем hyperlane core deploy..."
+            hyperlane core deploy
+        fi
+        if echo "$line" | grep -q "✅ Core contract deployments complete:"; then
+            echo "Настраиваем агента Hyperlane..."
+            hyperlane registry agent-config --chains base
 
-    # Настройка конфигурации Hyperlane
-    echo "Настраиваем конфигурацию Hyperlane..."
-    hyperlane core init --advanced
-
-    # Проверка успешного завершения
-    if grep -q "✅ Core contract deployments complete:" <<< "$(hyperlane core init --advanced)"; then
-        echo "Настройка агента Hyperlane..."
-        export CONFIG_FILES=$HOME/configs/agent-config.json
-        mkdir -p /tmp/hyperlane-validator-signatures-base
-        export VALIDATOR_SIGNATURES_DIR=/tmp/hyperlane-validator-signatures-base
-        mkdir -p $VALIDATOR_SIGNATURES_DIR
-    fi
+            export CONFIG_FILES=$HOME/configs/agent-config.json
+            mkdir -p /tmp/hyperlane-validator-signatures-base
+            export VALIDATOR_SIGNATURES_DIR=/tmp/hyperlane-validator-signatures-base
+            mkdir -p $VALIDATOR_SIGNATURES_DIR
+        fi
+    done
+    echo "Настройка ноды завершена."
 }
 
-# Функция для просмотра ключа SSH
-view_ssh_key() {
-    # Генерация SSH ключа
+# Функция для генерации SSH ключа
+generate_ssh_key() {
     echo "Генерация SSH ключа..."
     ssh-keygen -t rsa -b 4096 -C Hyperlane
+    cat ~/.ssh/id_rsa.pub
+}
 
-    # Проверка успешного завершения
-    if grep -q "SHA256" <<< "$(ssh-keygen -t rsa -b 4096 -C Hyperlane)"; then
-        sleep 5
-        cat ~/.ssh/id_rsa.pub
-        sleep 120
-        git clone git@github.com:hyperlane-xyz/hyperlane-monorepo.git
-    fi
+# Функция для запуска ноды
+start_node() {
+    read -p "Введите ваш приватный ключ EVM: " private_key
+    echo "Запускаем ноду..."
+    git clone git@github.com:hyperlane-xyz/hyperlane-monorepo.git
+    screen -S hyperlane
+    cd hyperlane-monorepo
+    cd rust
+    cd main
+    cargo run --release --bin validator -- \
+        --db ./hyperlane_db_validator_base\
+        --originChainName base\
+        --checkpointSyncer.type localStorage \
+        --checkpointSyncer.path $VALIDATOR_SIGNATURES_DIR \
+        --validator.key "0x${private_key}"
 }
 
 # Функция для просмотра логов
@@ -87,16 +94,19 @@ remove_node() {
 
 # Основное меню
 PS3="Выберите действие: "
-options=("Установить и запустить ноду" "Просмотр ключа SSH" "Просмотр логов" "Удалить ноду" "Выход")
+options=("Установить и настроить ноду" "Генерация ключа SSH" "Запустить ноду" "Просмотр логов" "Удалить ноду" "Выход")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Установить и запустить ноду")
+        "Установить и настроить ноду")
             install_git
-            install_and_run_node
+            install_and_configure_node
             ;;
-        "Просмотр ключа SSH")
-            view_ssh_key
+        "Генерация ключа SSH")
+            generate_ssh_key
+            ;;
+        "Запустить ноду")
+            start_node
             ;;
         "Просмотр логов")
             view_logs
